11d777f0d1d5915d96560b6745aa1db86309a056
Valeria Sabillón <vsabillon@unah.hn>
2021-06-07 22:25:30 -0600

    Creando Programa Rentas

---
+++ Ventas/Rentas/Win.Rentas/FormLogin.cs
@@ -0,0 +1,51 @@
+﻿using BL.Rentas;
+using System;
+using System.Windows.Forms;
+
+namespace Win.Rentas
+{
+    public partial class FormLogin : Form
+    {
+        SeguridadBL _seguridad;
+
+        public FormLogin()
+        {
+            InitializeComponent();
+            _seguridad = new SeguridadBL();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario;
+            string contrasena;
+
+            usuario = textBox1.Text;
+            contrasena = textBox2.Text;
+            var resultado= _seguridad.Autorizar(usuario, contrasena);
+
+            if (resultado==true)
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contrasena incorrecta");
+            }
+        }
+    }
+}
