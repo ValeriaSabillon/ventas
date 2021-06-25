a4683cfcac82979deca612ff69a19c8213f46321
Valeria Sabillón <vsabillon@unah.hn>
2021-06-24 18:44:14 -0600

    Agregando cambios al proyecto

---
+++ Ventas/Ventas_Proyecto/Win.Ventas/FormHombres.cs
@@ -0,0 +1,26 @@
+﻿using BL.Ventas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Ventas
+{
+    public partial class FormHombres : Form
+    {
+        HombresBL _product;
+
+        public FormHombres()
+        {
+            InitializeComponent();
+            _product = new HombresBL();
+            listaProdHombresBindingSource.DataSource = _product.obtener_Productos();
+
+        }
+    }
+}
---
+++ Ventas/Ventas_Proyecto/Win.Ventas/FormMujeres.cs
@@ -0,0 +1,26 @@
+﻿using BL.Ventas;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Ventas
+{
+    public partial class FormMujeres : Form
+    {
+        MujeresBL _productos;
+
+        public FormMujeres()
+        {
+            InitializeComponent();
+
+            _productos = new MujeresBL();//Inicializar
+            listaProdMujeresBindingSource.DataSource = _productos.obtenerProductos();
+        }
+    }
+}
